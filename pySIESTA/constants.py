cbfda00c9357acaedd14d794884786a3cc4a91d8
Raúl Coterillo <raulcote98@gmail.com>
2021-04-05 12:14:39 +0200

    auto-commit

--- pySIESTA/constants.py
+++ pySIESTA/constants.py
@@ -1,7 +1,13 @@
 """
-    Constants for their use throughout the program
+    Common information for its use throughout the program
 """
 
 ang2bohr = 1.88971616   # angstrom to bohr
 e2C = 1.60217646e-19    # elemental charge to Coulomb
 bohr2m = 5.29177e-11    # bohrs to meters
+
+ptable = {
+    8:  "O",
+    22: "Ti",
+    38: "Sr"
+}
